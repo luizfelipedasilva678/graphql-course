61c9c39c1b3966486bb4593bea82e63102c19023
Luiz Felipe <luizfelipedasilva678@gmail.com>
2022-04-11 20:56:07 -0300

    Resolvers with args

--- src/graphql/user/resolvers.js
+++ src/graphql/user/resolvers.js
@@ -1,7 +1,7 @@
 export const userResolvers = {
   Query: {
-    user: async (_, __, { fetch }) => {
-      const user = await fetch('http://localhost:3000/users/602');
+    user: async (_, { id }, { fetch }) => {
+      const user = await fetch(`http://localhost:3000/users/${id}`);
       return user.json();
     },
     users: async (_, __, { fetch }) => {
