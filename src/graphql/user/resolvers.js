7d56c6e678dadd09a56c4ce23f2953f7117e8026
Luiz Felipe <luizfelipedasilva678@gmail.com>
2022-04-11 20:49:43 -0300

    Resolvers

--- src/graphql/user/resolvers.js
+++ src/graphql/user/resolvers.js
@@ -1,26 +1,12 @@
 export const userResolvers = {
   Query: {
-    user: () => {
-      return {
-        id: '1',
-        userName: 'Luiz',
-      };
+    user: async (_, __, { fetch }) => {
+      const user = await fetch('http://localhost:3000/users/602');
+      return user.json();
     },
-    users: () => {
-      return [
-        {
-          id: '1',
-          userName: 'Luiz',
-        },
-        {
-          id: '2',
-          userName: 'Luiz',
-        },
-        {
-          id: '3',
-          userName: 'Luiz',
-        },
-      ];
+    users: async (_, __, { fetch }) => {
+      const users = await fetch('http://localhost:3000/users');
+      return users.json();
     },
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import { ApolloServer } from 'apollo-server';
 import { typeDefs, resolvers } from './graphql/schema';
+import fetch from 'node-fetch';
 
 const server = new ApolloServer({
   typeDefs: typeDefs,
   resolvers: resolvers,
+  context: () => {
+    return {
+      fetch,
+    };
+  },
 });
 
 server.listen(4000).then(({ url }) => {
