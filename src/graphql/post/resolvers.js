ef6d23ceafca58cb0657f1a64cc9e17708444804
Luiz Felipe <luizfelipedasilva678@gmail.com>
2022-04-11 21:28:39 -0300

    Posts config

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -1,26 +1,13 @@
 export const postResolvers = {
   Query: {
-    post: () => {
-      return {
-        id: '1',
-        postName: 'Post 1',
-      };
+    post: async (_, { id }, { getPosts }) => {
+      const response = await getPosts('/' + id);
+      return response.json();
     },
-    posts: () => {
-      return [
-        {
-          id: '1',
-          postName: 'Post 1',
-        },
-        {
-          id: '2',
-          postName: 'Post 2',
-        },
-        {
-          id: '3',
-          postName: 'Post 3',
-        },
-      ];
+
+    posts: async (_, __, { getPosts }) => {
+      const response = await getPosts();
+      return response.json();
     },
   },
 };
